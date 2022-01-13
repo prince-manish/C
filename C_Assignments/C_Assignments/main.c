8a9a259b2a1c33a018a9859d2a96857c587fbc1d
Mr MANISH CHAUHAN <manish@Mrs-MacBook-Air.local>
2022-01-13 16:18:28 +0530

    C_Assignments first commit

---
+++ C_Assignments/C_Assignments/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_Assignments
+//
+//  Created by Mr MANISH CHAUHAN on 13/01/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
